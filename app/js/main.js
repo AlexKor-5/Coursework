761f7922485ae9491c8444839328fb650d6b70fb
Alexander Korniichuk <world.kor.films@gmail.com>
2021-05-11 22:14:22 +0300

    editor development

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,13 @@ const init = () => {
     const ed_start_bth = document.getElementById('editor_start');
     const ed_finish_btn = document.getElementById('editor_finish');
     const ed_save_btn = document.getElementById('editor_save');
+
     let editor_switch = false;
     let truly = true;
 
     const names = document.querySelectorAll('[data-edit="name"]');
     const descriptions = document.querySelectorAll('[data-edit="description"]');
+    const images = document.querySelectorAll('[data-edit="image"]');
 
     const display_editor = (event) => {
         let elem = event.currentTarget;
@@ -29,26 +31,46 @@ const init = () => {
         });
     }
 
+    const links_life = (value = '', images = undefined) => {
+        if (images === undefined) return;
+        images.forEach((item) => {
+            let formElement = item.parentNode;
+            if (formElement.classList.contains('display-none')) {
+                if (value === 'alive'){
+                    formElement.classList.remove('display-none');
+                }
+            }else{
+                if((value === 'dead')){
+                    formElement.classList.add('display-none');
+                }
+            }
+        });
+    }
+
     const turnON_editor = () => {
-        if (names.length == 0 & descriptions.length == 0) return;
+        if (names.length === 0 && descriptions.length === 0) return;
         if (names.length !== 0 || descriptions.length !== 0) {
             truly = true;
             setter_true_false(names, truly);
             setter_true_false(descriptions, truly);
         }
+        links_life('alive', images);
     }
 
     const turnOFF_editor = () => {
         truly = false;
         setter_true_false(names, truly);
         setter_true_false(descriptions, truly);
+        links_life('dead', images);
     }
 
-    const main = (() => {
+    const main = () => {
         editor.addEventListener('click', display_editor, false);
         ed_start_bth.addEventListener('click', turnON_editor, false);
-        ed_finish_btn.addEventListener('click',turnOFF_editor,false);
+        ed_finish_btn.addEventListener('click', turnOFF_editor, false);
         // console.log(names.length);
-    })();
+    };
+    main();
+
 }
 init();
